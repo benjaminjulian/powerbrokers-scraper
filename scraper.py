7990683c83b9ec5ddb637c0c22d90725f1d236e6
benjaminjulian <benjaminjulian@protonmail.com>
2020-10-18 18:30:38 +0000

    Update scraper.py
    Núna líka foreldrar

--- scraper.py
+++ scraper.py
@@ -37,6 +37,10 @@ def fetchMOP(id): # Sækja grunngögn um þingmann
 		text = els[0].text.replace("(", "").replace(")", "")
 		reBirthDeath = re.compile(r"(Fædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}), dáin[n]? ([0-9]{1,2}). (.*?) ([0-9]{4})")
 		reBirth = re.compile(r"(Fædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4})")
+		reParents = re.compile(r".*?[Ff]oreldrar:(.*?)([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}), dáin[n]? ([0-9]{1,2}). (.*?) ([0-9]{4}).*? og.*?(([AÁBCDEFGHIÍJKLMNOÓPQRSTUÚVXYÝZÞÆÖ].*? ){1,4}).*?([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}), dáin[n]? ([0-9]{1,2}). (.*?) ([0-9]{4})")
+		reParentsFirstAlive = re.compile(r".*?[Ff]oreldrar:(.*?)([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}).*? og.*?(([AÁBCDEFGHIÍJKLMNOÓPQRSTUÚVXYÝZÞÆÖ].*? ){1,4}).*?([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}), dáin[n]? ([0-9]{1,2}). (.*?) ([0-9]{4})")
+		reParentsSecondAlive = re.compile(r".*?[Ff]oreldrar:(.*?)([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}), dáin[n]? ([0-9]{1,2}). (.*?) ([0-9]{4}).*? og.*?(([AÁBCDEFGHIÍJKLMNOÓPQRSTUÚVXYÝZÞÆÖ].*? ){1,4}).*?([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4})")
+		reParentsBothAlive = re.compile(r".*?[Ff]oreldrar:(.*?)([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4}).*? og.*?(([AÁBCDEFGHIÍJKLMNOÓPQRSTUÚVXYÝZÞÆÖ].*? ){1,4}).*?([Ff]ædd[u]?).{0,40}? ([0-9]{1,2})\. (.{1,10}) ([0-9]{4})")
 
 		if reBirthDeath.match(text):
 			grps = reBirthDeath.match(text).groups()
@@ -53,9 +57,96 @@ def fetchMOP(id): # Sækja grunngögn um þingmann
 
 		if len(grps) > 4:
 			res_death = grps[6] + "-" + getMonth(grps[5]) + "-" + pad(grps[4])
-			return (res_name, res_gender, res_birth, res_death)
 		else:
-			return (res_name, res_gender, res_birth)
+			res_death = "0000-00-00"
+		
+		if reParents.match(text):
+			grps = reParents.match(text).groups()
+
+			if len(grps[1]) == 4:
+				res_parent1_gender = "kvk"
+			else:
+				res_parent1_gender = "kk"
+
+			if len(grps[10]) == 4:
+				res_parent2_gender = "kvk"
+			else:
+				res_parent2_gender = "kk"
+
+			res_parent1_birth = grps[4] + "-" + getMonth(grps[3]) + "-" + pad(grps[2])
+			res_parent1_death = grps[7] + "-" + getMonth(grps[6]) + "-" + pad(grps[5])
+			res_parent1_name = grps[0].strip()
+			res_parent2_birth = grps[13] + "-" + getMonth(grps[12]) + "-" + pad(grps[11])
+			res_parent2_death = grps[16] + "-" + getMonth(grps[15]) + "-" + pad(grps[14])
+			res_parent2_name = grps[8].strip()
+
+			return ((res_name, res_gender, res_birth, res_death), (res_parent1_name, res_parent1_gender, res_parent1_birth, res_parent1_death), (res_parent2_name, res_parent2_gender, res_parent2_birth, res_parent2_death))
+		elif reParentsFirstAlive.match(text):
+			grps = reParentsFirstAlive.match(text).groups()
+
+			if len(grps[1]) == 4:
+				res_parent1_gender = "kvk"
+			else:
+				res_parent1_gender = "kk"
+
+			if len(grps[10]) == 4:
+				res_parent2_gender = "kvk"
+			else:
+				res_parent2_gender = "kk"
+
+			res_parent1_birth = grps[4] + "-" + getMonth(grps[3]) + "-" + pad(grps[2])
+			res_parent1_death = "0000-00-00"
+			res_parent1_name = grps[0].strip()
+			res_parent2_birth = grps[10] + "-" + getMonth(grps[9]) + "-" + pad(grps[8])
+			res_parent2_death = grps[13] + "-" + getMonth(grps[12]) + "-" + pad(grps[11])
+			res_parent2_name = grps[5].strip()
+
+			return ((res_name, res_gender, res_birth, res_death), (res_parent1_name, res_parent1_gender, res_parent1_birth, res_parent1_death), (res_parent2_name, res_parent2_gender, res_parent2_birth, res_parent2_death))
+		elif reParentsSecondAlive.match(text):
+			grps = reParentsSecondAlive.match(text).groups()
+
+			if len(grps[1]) == 4:
+				res_parent1_gender = "kvk"
+			else:
+				res_parent1_gender = "kk"
+
+			if len(grps[10]) == 4:
+				res_parent2_gender = "kvk"
+			else:
+				res_parent2_gender = "kk"
+
+			res_parent1_birth = grps[4] + "-" + getMonth(grps[3]) + "-" + pad(grps[2])
+			res_parent1_death = grps[7] + "-" + getMonth(grps[6]) + "-" + pad(grps[5])
+			res_parent1_name = grps[0].strip()
+			res_parent2_birth = grps[13] + "-" + getMonth(grps[12]) + "-" + pad(grps[11])
+			res_parent2_death = "0000-00-00"
+			res_parent2_name = grps[8].strip()
+
+			return ((res_name, res_gender, res_birth, res_death), (res_parent1_name, res_parent1_gender, res_parent1_birth, res_parent1_death), (res_parent2_name, res_parent2_gender, res_parent2_birth, res_parent2_death))
+		elif reParentsBothAlive.match(text):
+			grps = reParentsBothAlive.match(text).groups()
+
+			if len(grps[1]) == 4:
+				res_parent1_gender = "kvk"
+			else:
+				res_parent1_gender = "kk"
+
+			if len(grps[10]) == 4:
+				res_parent2_gender = "kvk"
+			else:
+				res_parent2_gender = "kk"
+
+			res_parent1_birth = grps[4] + "-" + getMonth(grps[3]) + "-" + pad(grps[2])
+			res_parent1_death = "0000-00-00"
+			res_parent1_name = grps[0].strip()
+			res_parent2_birth = grps[10] + "-" + getMonth(grps[9]) + "-" + pad(grps[8])
+			res_parent2_death = "0000-00-00"
+			res_parent2_name = grps[5].strip()
+
+			return ((res_name, res_gender, res_birth, res_death), (res_parent1_name, res_parent1_gender, res_parent1_birth, res_parent1_death), (res_parent2_name, res_parent2_gender, res_parent2_birth, res_parent2_death))
+		else:
+			print("failure")
+			return (res_name, res_gender, res_birth, res_death)
 	else:
 		return None
 
@@ -63,4 +154,9 @@ for i in range(1, 10):
 	print(i)
 	res = fetchMOP(i)
 	if res != None:
-		print(res)
+		if len(res) == 3:
+			print(res[0])
+			print("-->", res[1])
+			print("-->", res[2])
+		else:
+			print(res)
